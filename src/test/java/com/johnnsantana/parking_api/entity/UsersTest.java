85e3796bed6ee0264657bf239ce73d8701460ea8
Jhônatan Santana <jhonatansantanati@gmail.com>
2025-04-26 19:23:47 -0300

    test: validate Entity User #5 #6 #8 #10

--- src/test/java/com/johnnsantana/parking_api/entity/UsersTest.java
+++ src/test/java/com/johnnsantana/parking_api/entity/UsersTest.java
@@ -24,13 +24,19 @@ public class UsersTest {
         final var actualUser = new User(1L, username, password, admin, createdAt, updatedAt, createdBy, modifiedBy);
 
         Assertions.assertNotNull(actualUser);
+        Assertions.assertNotNull(actualUser.getId());
+
+        Assertions.assertEquals(username, actualUser.getUsername());
+        Assertions.assertEquals(password, actualUser.getPassword());
+        Assertions.assertEquals(admin, actualUser.getRole());
+
+        Assertions.assertNotNull(actualUser.getCreatedAt());
+        Assertions.assertNotNull(actualUser.getUpdatedAt());
+
+        Assertions.assertEquals(createdBy, actualUser.getCreatedBy());
+        Assertions.assertEquals(modifiedBy, actualUser.getModifiedBy());
+
 
     }
 
-    // Instancia
-
-    // Construtor
-    // Getter e Setters
-    // Enum
-    // LocalDatetime
 }
